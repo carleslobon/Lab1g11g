9edc5bc86232c9ac382c2aca55b765a574865df8
Gery345 <149507437+Gery345@users.noreply.github.com>
2024-10-06 14:04:39 +0200

    Actualització c0.py

--- chroma/c0.py
+++ chroma/c0.py
@@ -1,20 +1,34 @@
 from datasets import load_dataset
 import chromadb
+from chromadb.config import Settings
 import time
 
 chroma_client = chromadb.Client()
 
 collection = chroma_client.get_or_create_collection(name="sentences")
-print(collection)
+print("Collection created or retrieved")
 
 ds = load_dataset("williamkgao/bookcorpus100mb")
 sentences = ds['train']['text'][:10000]
+print("Dataset loaded")
 
 ids = [f"id{i}" for i in range(len(sentences))]
 
-start_time = time.time()
-for sentence, id in zip(sentences, ids):
-    collection.upsert(documents=[sentence], ids=[id])
-end_time = time.time()
+batch_size = 100
 
-print(f"Total insertion time: {end_time - start_time} seconds")
+print("Starting insertion process...")
+start_time = time.time()
+
+for i in range(0, len(sentences), batch_size):
+    print(f"Preparing batch {i // batch_size + 1}")
+    batch_sentences = sentences[i:i + batch_size]
+    batch_ids = ids[i:i + batch_size]
+    print(f"Batch {i // batch_size + 1} ready for insertion")
+    try:
+        collection.upsert(documents=batch_sentences, ids=batch_ids)
+    except Exception as e:
+        print(f"Error: {e}")
+    print(f"Batch {i // batch_size + 1} inserted")
+
+end_time = time.time()
+print(f"Insertion completed in {end_time - start_time} seconds")
